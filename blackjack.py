186c002ad4898864500dbc4568e63723b2edec38
sheetalbongale <57145360+sheetalbongale@users.noreply.github.com>
2020-02-23 23:59:16 -0600

    Set up

---
+++ blackjack.py
@@ -0,0 +1,20 @@
+import random import shuffle
+import sys
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+
+faces = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':10,
+         'Q':10, 'K':10, 'A': [1, 11]}
+
+class Card:
+
+class Player:
+
+
+class Dealer:
+
+class Game:
+
+class Deck:
+
+class
